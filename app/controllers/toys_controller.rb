b8e3b03fe0e61619f904aef064444ef6fef2633e
Yuyi <yuyi365@gmail.com>
2021-12-14 11:23:09 -0500

    commit tree 6c224f7a8ba0cb7544c681732c470f97d78d5846

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,4 +1,5 @@
 class ToysController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   wrap_parameters format: []
 
   def index
@@ -7,25 +8,32 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.update(toy_params)
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.destroy
     head :no_content
   end
 
   private
-  
+
+  def find_toy
+    Toy.find(params[:id])
+  end
+
   def toy_params
     params.permit(:name, :image, :likes)
   end
 
+  def render_not_found
+    render json: { error: 'toy not found' }, status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :toys, only: [:index, :create, :update]
+  resources :toys, only: %i[index create update destroy]
 end
